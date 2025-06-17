53df23c2f532184b4eb1e7c99c4ac68fd66e94db
Preeti Gurung <gurungpreeti999@gmail.com>
2025-06-17 12:00:10 +0545

    updated index2

---
+++ app/index2.js
@@ -0,0 +1,18 @@
+const styles = StyleSheet.create({
+  container: { padding: 20, alignItems: 'center' },
+  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 },
+  status: { marginBottom: 20, fontSize: 16 },
+  relayBox: {
+    marginBottom: 20,
+    padding: 20,
+    width: '100%',
+    backgroundColor: '#f0f0f0',
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  relayText: {
+    marginBottom: 10,
+    fontSize: 18,
+    fontWeight: '600',
+  },
+});
